54eca98bf06fb8515f9442753d56597783d9e452
CFabian <fabian_coll@bentley.edu>
2020-12-02 17:42:24 -0500

    d

--- app.py
+++ app.py
@@ -67,7 +67,12 @@ try:
 except:
     st.write('Please choose a valid stock.')
 
-st.write('As we can below, our data consists of daily prices including the daily: open, high, low, close and return. For our return statistic. We are using the formula below, where P(i) = todays returns and P(i-1) = yesterdays returns')
+st.write('''
+    As we can below, our data consists of daily prices including the daily: 
+    open, high, low, close and return. For our return statistic. We are using the 
+    formula below, where P(i) = todays returns and P(i-1) = yesterdays returns.
+    ''')
+
 post_image('daily_return.jpg', 'Daily return formula')
 
 # Used as a command to improve processing speed
