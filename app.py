8f76acff2cb6213259353a31ae73aea74bc2fe6f
CFabian <fabian_coll@bentley.edu>
2020-12-03 08:54:09 -0500

    d

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def make_summary_table(y_col):
         'Std' : np.std(y_col),
         'Skew' : skew(y_col)
     }
-    dat = pd.DataFrame.from_dict(stat_dict, index = [0])
+    dat = pd.DataFrame(stat_dict, index = [0])
     st.table(dat)
 
 
